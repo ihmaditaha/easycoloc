f507dbbf5e620504d96340315b0cbf8ca7dcb517
ihmaditaha <ihmaditaha1@gmail.com>
2026-02-26 10:20:38 +0000

    Add initial application structure with routes, tests, and configuration files

---
+++ database/migrations/2026_02_24_114722_create_joined_colocations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Carbon\Traits\Timestamp;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('memberships', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('colocation_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->enum('role',['member','owner'])->default('member');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('left_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('joined_colocations');
+    }
+};
